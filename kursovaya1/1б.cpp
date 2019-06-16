63f9c0ac61d700a7df4ea497def7ece37b495ea9
Rrrozya <36341772+Rrrozya@users.noreply.github.com>
2019-06-17 01:09:53 +0300

    Update 1б.cpp

--- kursovaya1/1б.cpp
+++ kursovaya1/1б.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     float x, y;
     cin >> y >> x;
-    float v = log(y + 0.95) + sin(pow(x, 4));
+    float v = float(log(y + 0.95) + sin(pow(x, 4)));
     cout << v;
     return 0;
 }
