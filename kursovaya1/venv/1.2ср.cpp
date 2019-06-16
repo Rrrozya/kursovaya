7fbdd0a809f6551aaa9b4247dc5dce9b635e0cc5
Rrrozya <36341772+Rrrozya@users.noreply.github.com>
2019-06-17 01:52:26 +0300

    Create 1.2ср.cpp

---
+++ kursovaya1/venv/1.2ср.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    float m = 1.8, k;
+    cin >> k;
+    float x = float(pow(exp(1.0), m*k));
+    cout << x;
+    float c = float(pow(cos(m), 2)+pow(k, 2));
+    cout << c;
+    float y = float(pow((pow(x, 2)+pow(c, 2)), 1/3));
+    cout << y;
+}
