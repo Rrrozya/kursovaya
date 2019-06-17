02eceb2e4fecd28cdf1d1a14b530187044a06770
Rrrozya <36341772+Rrrozya@users.noreply.github.com>
2019-06-17 03:51:58 +0300

    Create 2ба.cpp

---
+++ kursovaya1/venv/2ба.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 1, y = 2;
+    if ((x*y >= 0) || (y > x)){
+        cout << "True";
+    } else {
+        cout << "False";
+    }
+}
